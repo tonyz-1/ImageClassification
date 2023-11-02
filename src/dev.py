ac4c29c0af14ad295eb9240c6a8388756d8175fd
Patrick Lister <patrickrlister@gmail.com>
2023-11-01 21:13:17 -0400

    Training code

--- src/dev.py
+++ src/dev.py
@@ -1,22 +1,81 @@
 from torchvision.datasets import CIFAR10
 from torch.utils.data import DataLoader
+import torchvision.transforms as transform
 from torch import optim
 import vanillaModel
+from prodigyopt import Prodigy
+import torch.nn as nn
+import torch
 
-dataset = CIFAR10('./data', download=True)
+import matplotlib.pyplot as plt
+
+device = 'cpu'
+encoder_file = './encoder.pth'
 
 # Hyper Parameters
-gamma = 1
-lr = 1e-4
-batch_size = 64
+batch_size = 512
+weight_decay = 0.1
+n_epochs = 10
 
 # Dataset
-image_dl = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+train_transform_exotic = transform.Compose([transform.RandomCrop(32, padding=4),
+                                     transform.RandomHorizontalFlip(),
+                                     transform.ToTensor(),
+                                     transform.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),])
+train_transform_vanilla = transform.Compose([transform.ToTensor()])
+dataset_train = CIFAR10('./data', download=True, train=True, transform=train_transform_vanilla)
+dataset_test = CIFAR10('./data', download=True, train=False, transform=train_transform_vanilla)
+train_dl = DataLoader(dataset_train, batch_size=batch_size, shuffle=True)
+test_dl = DataLoader(dataset_train, batch_size=batch_size, shuffle=False)
 
 # Model Instantiation
-model = vanillaModel.vggClassifier.vgg()
+backend = vanillaModel.vggClassifier
+backend.vgg.to(device)
+backend.vgg.load_state_dict(torch.load(encoder_file, map_location=device))
+model = vanillaModel.VanillaModel(backend)
+model.train()
+
+#baseline_model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=False)
+#baseline_model.to(device)
+#baseline_model.train()
 
 # Optimizer and Scheduler
-optimizer = optim.Adam(model.parameters(), lr=lr)
-scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma)
+optimizer = Prodigy(model.parameters(), lr=1., weight_decay=weight_decay, decouple=True)
+scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=n_epochs)
 
+loss_fn = torch.nn.CrossEntropyLoss()
+
+losses_list = []
+for epoch in range(n_epochs):
+    loss_epoch = 0.0
+    index = 0
+    for imgs, labels in train_dl:
+        index += 1
+        input_ims = imgs.to(device)
+        labels = labels.to(device)
+
+        optimizer.zero_grad()
+
+        #output = baseline_model(input_ims)
+        #loss = loss_fn(output, labels)
+
+        output = model.forward(input_ims)
+        loss = model.loss(output, labels)
+
+        loss.backward()
+        optimizer.step()
+
+        loss_epoch += loss.item()
+        if index % 5 == 0:
+            print(f"Batch {index} loss: {loss}")
+    losses_list.append(loss_epoch)
+    scheduler.step()
+    print(f"Epoch {epoch} loss: {losses_list[epoch]}")
+    if epoch % 5 == 0:
+        state_dict = model.classifier.state_dict()
+        torch.save(state_dict, f"./weights/weight_epoch_{epoch}.pth")
+
+
+plt.plot(losses_list, label="Loss")
+plt.xlabel("Epoch")
+plt.ylabel("Loss")
--- src/vanillaModel.py
+++ src/vanillaModel.py
@@ -1,6 +1,7 @@
 import torch.nn as nn
+import torch
 
-class vggClassifier:
+class vggClassifier(nn.Module):
     vgg = nn.Sequential(
         nn.Conv2d(3, 3, (1, 1)),
         nn.ReflectionPad2d((1, 1, 1, 1)),
@@ -34,10 +35,38 @@ class vggClassifier:
         nn.Conv2d(256, 512, (3, 3)),
         nn.ReLU(),  # relu4-1, this is the last layer used
     )
-
     classifier = nn.Sequential(
+        nn.Conv2d(512, 256, 1),
+        nn.Conv2d(256, 128, 1),
         nn.Flatten(),
-        nn.Linear(),
-        nn.ReLU(),
-        nn.Linear()
+        nn.Linear(2048, 10)
     )
+    vggFrontend = nn.Sequential(
+
+    )
+
+class VanillaModel(nn.Module):
+    def __init__(self, model):
+        super(VanillaModel, self).__init__()
+        self.encoder = model.vgg
+        self.classifier = model.classifier
+        for param in self.classifier.parameters():
+            nn.init.normal_(param, mean=0, std=0.01)
+        self.loss = nn.CrossEntropyLoss()
+        for param in self.encoder.parameters():
+            param.requires_grad = False
+
+    def encode(self, X):
+        bottleneck = self.encoder(X)
+        test = bottleneck.shape
+        return bottleneck
+
+    def decode(self, X):
+        return self.classifier(X)
+    
+    def forward(self, X, train=True):
+        encoded_result = self.encode(X)
+        output_tensor = self.decode(encoded_result)
+        sm = nn.Softmax(dim=1)
+        classified = sm(output_tensor)
+        return classified
